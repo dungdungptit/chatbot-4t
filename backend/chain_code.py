b0e240c51d9c7ad04259f24d6efb28bf0903078c
dungdungptit <anhdungvk01@gmail.com>
2024-10-18 10:16:30 +0700

    fix question vietnamese

--- backend/chain_code.py
+++ backend/chain_code.py
@@ -363,7 +363,8 @@ template_vietnamese_fusion = """Bạn là một tư vấn viên chuyên nghiệp
 về các thông tin về các standarzation.
 Bạn có thể tạo ra nhiều truy vấn tìm kiếm dựa trên một truy vấn đầu vào duy nhất. \n
 Tạo ra nhiều truy vấn tìm kiếm liên quan đến: {question} \n
-Đầu ra (3 truy vấn):"""
+Lưu ý đầu ra trả về các truy vấn tiếng Anh nhé
+Đầu ra (3 truy vấn tiếng Anh):"""
 
 prompt_rag_fusion = ChatPromptTemplate.from_template(template_vietnamese_fusion)
 
@@ -373,6 +374,14 @@ generate_queries = (
     | StrOutputParser()
     | (lambda x: x.split("\n"))
 )
+
+translate_prompt_template = """Bạn là 1 người giỏi tiếng anh và các ngôn ngữ khác bao gồm cả tiếng Việt. Với đầu vào sau đây: \n{question}Nếu câu đầu vào là tiếng Việt, hãy dịch trả về là tiếng Anh.
+Nếu câu đầu vào là tiếng Anh, hãy trả về là tiếng Anh.
+Đầu ra (Tiếng Anh):"""
+
+translate_prompt = ChatPromptTemplate.from_template(translate_prompt_template)
+
+translate_query = translate_prompt | ChatOpenAI(temperature=0) | StrOutputParser()
 
 from langchain.load import dumps, loads
 
@@ -419,10 +428,18 @@ def remove_duplicate_words(text: str):
 def get_results(question: str):
     question = normalize_replace_abbreviation_text(question)
     docs = retrieval_chain_rag_fusion.invoke({"question": question})
+    question = translate(question=question)
     docs1 = retriever.get_relevant_documents(question)
     docs.append(docs1)
     docs = reciprocal_rank_fusion(docs)
     return docs
+
+
+def translate(question: str):
+    question = normalize_replace_abbreviation_text(question)
+    question = translate_query.invoke({"question": question})
+    print("===============================", question)
+    return question
 
 
 retrieval_chain_rag_fusion = generate_queries | retriever.map()
